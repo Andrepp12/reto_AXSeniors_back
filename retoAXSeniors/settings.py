b63d6d46d0f0a08494e3def8e9291cb0169fd58f
Andrepp12 <73384545cometa@gmail.com>
2025-05-03 13:08:31 -0500

    Add front to allowed hosts

--- retoAXSeniors/settings.py
+++ retoAXSeniors/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('FRONTEND_URL', 'http://localhost'),
+                 ]
 
 
 # Application definition
